f6ad87a01553b8645891d5c9f3190c948dffff50
Homan <homanwang@hotmail.com>
2022-06-03 14:19:19 +0800

    Add spring matching-strategy

--- src/main/java/com/example/DemoProject2Application.java
+++ src/main/java/com/example/DemoProject2Application.java
@@ -1,6 +1,5 @@
 package com.example;
 
-import com.example.controller.UserController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +9,5 @@ public class DemoProject2Application {
     public static void main(String[] args) {
         SpringApplication.run(DemoProject2Application.class, args);
     }
-
 
 }
